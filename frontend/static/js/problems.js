bb6d53ca4077ac91576260325411a4c8955752e7
Kevin <kevinzhenng@gmail.com>
2018-09-09 00:16:19 -0400

    Add better error handling in list problems page

--- frontend/static/js/problems.js
+++ frontend/static/js/problems.js
@@ -19,6 +19,15 @@ window.onload = function() {
         throw new Error(res.error);
     }
 
+    const msg = document.querySelector(".message");
+    function error(err) {
+        if (err instanceof SyntaxError) {
+            err = new Error("Service unavailable.");
+        }
+
+        msg.innerText = err;
+    }
+
     function populateProblems(problems) {
         for (const p of problems) {
             const el = createProblem(p.id, p.name);
@@ -27,11 +36,16 @@ window.onload = function() {
             const br = document.createElement("br");
             problemsContainer.appendChild(br);
         }
+
+        const content = document.querySelector(".content");
+        content.classList.remove("hidden");
+
+        msg.classList.add("hidden"); 
     }
 
     fetch("/api/problems/list")
     .then(res => res.json())
     .then(handleError)
     .then(populateProblems)
-    .catch(err => alert(err));
+    .catch(error);
 }
