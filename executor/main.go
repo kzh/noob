4017bcee5122f3d5594fda70370a858a78ae3028
Kevin <kevinzhenng@gmail.com>
2018-09-29 06:43:20 -0400

    Add dynamically created container execution

--- executor/main.go
+++ executor/main.go
@@ -5,11 +5,13 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/streadway/amqp"
 
@@ -73,6 +75,67 @@ func buildImage(id string, buildContext io.Reader) error {
 	return nil
 }
 
+func prepareContainer(uid string) (string, error) {
+	ctx := context.Background()
+	resp, err := dock.ContainerCreate(ctx, &container.Config{
+		Image: uid,
+	}, nil, nil, "")
+	if err != nil {
+		return "", err
+	}
+
+	err = dock.ContainerStart(
+		ctx, resp.ID,
+		types.ContainerStartOptions{},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return resp.ID, nil
+}
+
+func test(uid, in, out string) (string, error) {
+	ctx := context.Background()
+	resp, err := dock.ContainerExecCreate(
+		ctx, uid,
+		types.ExecConfig{
+			Cmd:          []string{"./ex"},
+			Tty:          true,
+			AttachStdin:  true,
+			AttachStderr: true,
+			AttachStdout: true,
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	exec, err := dock.ContainerExecAttach(
+		ctx, resp.ID,
+		types.ExecStartCheck{Tty: true},
+	)
+	if err != nil {
+		return "", err
+	}
+	defer exec.Close()
+
+	_, err = exec.Conn.Write([]byte(in))
+	if err != nil {
+		return "", err
+	}
+
+	n, err := fmt.Fscanf(exec.Reader, in+out)
+	log.Printf("%d %#v", n, err)
+
+	/*
+		line, err := ioutil.ReadAll(exec.Reader)
+		log.Printf("%s\n%#v\n", string(line), line)
+	*/
+
+	return "", err
+}
+
 func handle(msg amqp.Delivery) {
 	var submission model.Submission
 	err := json.Unmarshal(msg.Body, &submission)
@@ -91,6 +154,18 @@ func handle(msg amqp.Delivery) {
 	}
 
 	err = buildImage(submission.ID, ctx)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	cid, err := prepareContainer(submission.ID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	_, err = test(cid, "100 100\n", "200")
 	if err != nil {
 		log.Println(err)
 		return
